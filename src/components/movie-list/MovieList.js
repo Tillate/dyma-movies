1a263566769e1083494008ed8687d12e229189b1
allan <allan.lelous@gmail.com>
2022-03-31 20:36:07 +0200

    initialisation du projet, bootstrap, création des 3 components (Header, MovieDetails, MovieList)

---
+++ src/components/movie-list/MovieList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+    render() {
+        return (
+            <div className='w-75'>
+                MovieList
+            </div>
+        );
+    }
+}
+
+export default MovieList;
