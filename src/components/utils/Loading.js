591beb93eab1d7b28d5c3f3a62116545e2733226
allan <allan.lelous@gmail.com>
2022-04-09 18:29:53 +0200

    Ajout d'un gif  au chargement de la page, création d'un composant pour stocker la data des films

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div style={ { minHeight: '50vh' } } className="d-flex flex-row justify-content-center align-content-center w-100">
+            <img src="https://cdn.dribbble.com/users/2973561/screenshots/5757826/loading__.gif" alt="Loader gif" />
+        </div>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+        {
+          title: 'The Godfather',
+          img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQyO8trmFbGTJIY6MaIFqzfPB6hW8UFCAYxTqtdKWGz7EJ6Jt-d',
+          details: 'R | 175 min | Crime, Drama',
+          description: "En 1945, à New York, les Corleone sont une des 5 familles de la mafia. Don Vito Corleone, `parrain' de cette famille, marie sa fille à un bookmaker."
+        }, {
+          title: 'Harry Potter et le prince de sang mêlé',
+          img: 'https://static.posters.cz/image/750webp/104623.webp',
+          details: 'R | 210 min | Fantastique, Drama',
+          description: "Cette sixième année scolaire de Harry Potter à l'école de sorciers commence par une dispute avec son ennemi juré Draco Malfoy, en qui les forces des ténèbres placent désormais leurs espoirs.."
+        }, {
+          title: 'Le Seigneur des anneaux',
+          img: 'https://antreducinema.fr/wp-content/uploads/2021/04/communaute.jpg',
+          details: 'R | 190 min | Aventure, Fantastique',
+          description: "Un jeune et timide `Hobbit', Frodon Sacquet, hérite d'un anneau magique. Bien loin d'être une simple babiole, il s'agit d'un instrument de pouvoir absolu qui permettrait à Sauron."
+        }, {
+          title: 'Sherlock Holmes',
+          img: 'https://www.cinema-florival.com/i/films/797g.jpg',
+          details: 'R | 220 min | Crime, Policier',
+          description: "Quand le prince autrichien est retrouvé mort, on pense à un suicide. Or le détective Sherlock Holmes pense qu'il a été assassiné et que le crime est une des pièces du puzzle conçu par le professeur Moriarty."
+        }
+      ]
