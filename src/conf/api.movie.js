3d848756b73155a059bede95e3c286d135710e3b
allan <allan.lelous@gmail.com>
2022-04-18 16:26:57 +0200

    Rajout de la search bar avec formik et mapping des recherches

--- src/conf/api.movie.js
+++ src/conf/api.movie.js
@@ -1,12 +1,19 @@
 import * as axios from 'axios';
 
 const apiMovie = axios.create({
-    baseURL: 'https://www.themoviedb.org/4'
+  baseURL: 'https://api.themoviedb.org/4'
 })
 
 apiMovie.interceptors.request.use( req => {
-    req.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGE0ZDk0YjNlZDkyYzMyZjU3YTUyOWU1ZDIyN2Q4YyIsInN1YiI6IjYyMmEyOWEyZDZjMzAwMDA2YzU2M2Y3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.E84irXFtoBbuc5sQTnkf6yLApsBdIpWTtX-k1s2RdLc'
-    return req;
+  req.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGE0ZDk0YjNlZDkyYzMyZjU3YTUyOWU1ZDIyN2Q4YyIsInN1YiI6IjYyMmEyOWEyZDZjMzAwMDA2YzU2M2Y3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.E84irXFtoBbuc5sQTnkf6yLApsBdIpWTtX-k1s2RdLc'
+  return req;
 })
 
 export default apiMovie;
+
+export const apiMovieMap = (m) => ({
+    img: 'https://image.tmdb.org/t/p/w500' + m.poster_path,
+    title: m.title,
+    details: m.release_date + ' | ' + m.vote_average + ' /10 (' + m.vote_count + ')',
+    description: m.overview
+})
